c1b7bb3bd45a8240955dd77bf7459bf56532dce6
JKimani77 <jkkimani77@gmail.com>
2021-06-06 17:18:00 +0300

    configured django-project settings and url paths

---
+++ hdapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path
+from . import views
+
+
+urlpatterns = [
+    #path('', views.home, name='home'),
+    #path('login/', views.login, name = 'login'),
+    #path('createprofile/', views.make_profile, name = 'createprofile'),
+    #re_path('profile/(?P<id>\d+)/',views.view_profile,name = 'myprofile'),
+    #path('profile/<int:id>/', views.view_profile, name = 'myprofile'),
+    #path('post/',views.posting_project, name='post'),
+    #path('logout/',views.logout_user, name='logout'),
+    #path('search/', views.search,name='search-project'),
+
+]
